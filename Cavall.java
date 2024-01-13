cb224ade20fd154f75c4e75c32df821c2d4b2e66
claraconstans <u1979264@campus.udg.edu>
2024-01-13 22:35:51 +0100

    Vector2 i funcions tipusVehicle

--- Cavall.java
+++ Cavall.java
@@ -2,10 +2,12 @@ class Cavall extends TipusVehicle{
     
 
     void accelerar(Vector2 pos, Vector2 vel, Vector2 acc){
-
+        vel.add(acc);
+        pos.add(vel);
     }
     void frenar(Vector2 pos, Vector2 vel, Vector2 acc){
-
+        vel.add(acc);
+        pos.add(vel);
     }
 
 }
