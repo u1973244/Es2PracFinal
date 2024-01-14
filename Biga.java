9be378ca928e8466a7b7530318907e85b1e386e2
claraconstans <u1979264@campus.udg.edu>
2024-01-14 23:32:29 +0100

    constructors tipus vehicles arreglats

--- Biga.java
+++ Biga.java
@@ -1,7 +1,7 @@
 class Biga extends TipusVehicle{
     
     Biga(int id, String nom, double vMax, double adherencia, double resistenciaAlXoc){
-        super(int id, String nom, double vMax, double adherencia, double resistenciaAlXoc);
+        super(id, nom, vMax, adherencia, resistenciaAlXoc);
     }
 
     void accelerar(double pos, double vel, double acc){
--- Quadriga.java
+++ Quadriga.java
@@ -1,7 +1,7 @@
 class Quadriga extends TipusVehicle{
     
     Quadriga(int id, String nom, double vMax, double adherencia, double resistenciaAlXoc){
-        super(int id, String nom, double vMax, double adherencia, double resistenciaAlXoc);
+        super(id, nom, vMax, adherencia, resistenciaAlXoc);
     }
 
     void accelerar(double pos, double vel, double acc){
